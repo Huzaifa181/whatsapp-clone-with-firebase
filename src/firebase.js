ef768c3835229031385e4fbbb1e4831db1d6b2e4
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-06-10 14:30:36 +0500

    Inialize firebase and with frontend

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,19 @@
+import firebase from 'firebase'
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCEJsLQXbOZPFDFAreRPfgo8k06PcYWFAM",
-    authDomain: "whatsapp-mern-8e7b9.firebaseapp.com",
-    projectId: "whatsapp-mern-8e7b9",
-    storageBucket: "whatsapp-mern-8e7b9.appspot.com",
-    messagingSenderId: "370988059731",
-    appId: "1:370988059731:web:7cd1d1e56563fbb3044aa6"
-  };
+  apiKey: "AIzaSyBsQ-co6SdfcxWkZAIB24wYWoBFk3z2Luo",
+  authDomain: "chatapp-ff5a3.firebaseapp.com",
+  databaseURL: "https://chatapp-ff5a3.firebaseio.com",
+  projectId: "chatapp-ff5a3",
+  storageBucket: "chatapp-ff5a3.appspot.com",
+  messagingSenderId: "427653757536",
+  appId: "1:427653757536:web:946d9e0a02d61b6c06e105",
+  measurementId: "G-52HJM487JG"
+};
+
+  const firebaseApp=firebase.initializaApp(firebaseConfig)
+  const db=firebaseApp.firestore();
+  const auth=firebaseApp.auth();
+  const provider=new firebase.auth.GoogleAuthProvider();
+  export {auth,provider}
+  export default db;
